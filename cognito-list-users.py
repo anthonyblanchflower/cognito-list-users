3dd7286c624ed562d368c760a5c99e445b846b13
anthonyblanchflower <anthony.blanchflower@comparethemarket.com>
2019-01-28 15:47:43 +0000

    add description of the user pool instance

--- cognito-list-users.py
+++ cognito-list-users.py
@@ -1,6 +1,8 @@
 from warrant import Cognito
+import boto3
 import argparse
 import sys
+import json
 
 args = ''
 
@@ -24,9 +26,35 @@ def get_args():
         return val
 
 
-def get_user_pool_list():
-    pool_args = get_args()
-    user_pool = Cognito(pool_args['user_pool_id'], pool_args['client_id'])
+def get_user_pool_dictionary(pool_id):
+
+    client = boto3.client('cognito-idp')
+
+    user_pool_dict = {}
+
+    user_pool_identity = client.describe_user_pool(UserPoolId=pool_id)
+    user_pool = user_pool_identity['UserPool']
+    user_pool_dict['name'] = user_pool['Name']
+    user_pool_dict['policies'] = user_pool['Policies']
+    user_pool_dict['created_on'] = str(user_pool['CreationDate'])
+    user_pool_dict['modified_on'] = str(user_pool['LastModifiedDate'])
+    user_pool_dict['auto_verified_attributes'] = user_pool['AutoVerifiedAttributes']
+    user_pool_dict['username_attributes'] = user_pool['UsernameAttributes']
+    user_pool_dict['verification_message_template'] = user_pool['VerificationMessageTemplate']
+    user_pool_dict['device_configuration'] = user_pool['DeviceConfiguration']
+    user_pool_dict['estimated_number_of_users'] = user_pool['EstimatedNumberOfUsers']
+    user_pool_dict['email_configuration'] = user_pool['EmailConfiguration']
+    user_pool_dict['tags'] = user_pool['UserPoolTags']
+    user_pool_dict['domain'] = user_pool['Domain']
+    user_pool_dict['admin_create_user_config'] = user_pool['AdminCreateUserConfig']
+    user_pool_dict['arn'] = user_pool['Arn']
+
+    return user_pool_dict
+
+
+def get_user_pool_list(pool_id, client_id):
+
+    user_pool = Cognito(pool_id, client_id)
     user_pool_list = user_pool.get_users()
 
     return user_pool_list
@@ -34,7 +62,19 @@ def get_user_pool_list():
 
 if __name__ == "__main__":
 
-    pool_list = get_user_pool_list()
+    pool_args = get_args()
+    user_pool_id = pool_args['user_pool_id']
+    pool_client_id = pool_args['client_id']
 
-    for pool_user in pool_list:
+    pool_dictionary = get_user_pool_dictionary(user_pool_id)
+
+    pool_user_list = get_user_pool_list(user_pool_id, pool_client_id)
+
+    print ("User Pool description : ")
+
+    print (json.dumps(pool_dictionary, indent=2))
+
+    print ("User Pool users : ")
+
+    for pool_user in pool_user_list:
         print (pool_user._data)
